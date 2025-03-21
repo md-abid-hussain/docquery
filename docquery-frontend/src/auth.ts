f1530e3d356b11604a049b05e760f2b83e20a334
Md Abid Hussain <abid0hussain1733@gmail.com>
2025-03-21 14:30:34 +0530

    Add Prisma adapter for NextAuth and update user authentication models

--- docquery-frontend/src/auth.ts
+++ docquery-frontend/src/auth.ts
@@ -1,8 +1,14 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  session:{
+    strategy: 'jwt',
+  },
   callbacks: {
     async jwt({ token, profile }) {
       if (profile) {
